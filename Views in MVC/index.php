dfc5e701ee1463007004df5f91c438bdaad4ec32
metin <kaan.yalim@httl.rennweg.at>
2025-03-05 15:08:53 +0100

    v1.1

--- Views in MVC/index.php
+++ Views in MVC/index.php
@@ -1,31 +1,19 @@
 <?php
 
 $hotels = [
-    [
-        'name' => 'Bellagio',
-        'description' => 'A luxurious hotel with a stunning fountain show.'
-    ],
-    [
-        'name' => 'Caesars Palace',
-        'description' => 'An iconic resort with Roman-themed architecture.'
-    ],
-    [
-        'name' => 'The Venetian',
-        'description' => 'A breathtaking Venice-themed experience.'
-    ],
-    [
-        'name' => 'MGM Grand',
-        'description' => 'A grand hotel with a lion habitat.'
-    ],
-    [
-        'name' => 'Wynn Las Vegas',
-        'description' => 'A high-end hotel with a golf course.'
-    ]
+    ['name' => 'Bellagio', 'description' => 'A luxurious hotel with a stunning fountain show.'],
+    ['name' => 'Caesars Palace', 'description' => 'An iconic resort with Roman-themed architecture.'],
+    ['name' => 'The Venetian', 'description' => 'A breathtaking Venice-themed experience.'],
+    ['name' => 'MGM Grand', 'description' => 'A grand hotel with a lion habitat.'],
+    ['name' => 'Wynn Las Vegas', 'description' => 'A high-end hotel with a golf course.']
 ];
 
-$templateFile = fopen('template.html', 'r');
-$template = fread($templateFile, filesize('template.html'));
-fclose($templateFile);
+$templateFile = 'template.html';
+if (!file_exists($templateFile)) {
+    die('Template file not found');
+}
+
+$template = file_get_contents($templateFile);
 
 $hotelItems = "";
 foreach ($hotels as $hotel) {
